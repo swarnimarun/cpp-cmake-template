ec9246a61043e748c8a30c7f277cdde14b76df18
Swarnim Arun <swarnimarun11@gmail.com>
2023-05-27 01:12:05 +0530

    add: clang format support

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+
 #include <fmt/core.h>
 #include <fmt/format.h>
 
-int main(int argc, char **argv) {
-  if (argc <= 1) {
-    std::cerr << "No argument provided!" << std::endl;
-    return -1;
-  }
-  std::cout << fmt::format("Hello, {}\n", argv[1]);
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        fmt::print(stderr, "No argument provided!");
+        return -1;
+    }
+    fmt::print("Hello, {}\n", argv[1]);
 }
