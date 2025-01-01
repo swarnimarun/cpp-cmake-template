f8f82aba1566190a0964881c3c69733446c3f99c
Swarnim Arun <swarnimarun11@gmail.com>
2025-01-02 04:35:44 +0530

    fix: cpp use front over begin

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 constexpr const int INVALID_ARGUMENTS = 1;
 
 int main(int argc, char** argv) {
-    auto first = std::ranges::subrange(argv, argv + argc).advance(1).begin();
-    if (*first == nullptr) {
+    auto first = std::ranges::subrange(argv, argv + argc).advance(1).front();
+    if (first == nullptr) {
         std::print(stderr, "No arguments passed");
         return INVALID_ARGUMENTS;
     }
-    std::print("Hello, {}", *first);
+    std::print("Hello, {}", first);
 }
